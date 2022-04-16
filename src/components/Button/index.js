29b4bc8bb08d682e90b5c4dacc5e324a429c20fb
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-04-16 22:31:11 +0700

    add props to button comp

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Button({ title, type }) {
+function Button({ title, type, bg, color, onPress }) {
   if (type === "success") {
     return (
       <button className="w-full mt-5 px-4 py-5 rounded-md text-center bg-green-500 shadow-lg text-white font-semibold mr-3">
@@ -9,7 +9,12 @@ function Button({ title, type }) {
     );
   }
   return (
-    <button className="px-4 py-2 rounded-md text-center bg-blue-500 shadow-lg text-white font-semibold mr-3">
+    <button
+      onClick={onPress}
+      className={`px-4 py-2 rounded-md text-center ${
+        bg ? bg : "bg-blue-500"
+      } shadow-lg text-white font-semibold mr-3`}
+    >
       {title}
     </button>
   );
