392a6707adf6feff94e07022d0e89980c33badfa
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-04-17 16:28:51 +0700

    add type btn order

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 
 function Button({ title, type, bg, color, onPress }) {
+  if (type === "btn-completed") {
+    return (
+      <button className="rounded-2xl bg-green-800 text-green-200 py-1 px-4">
+        {title}
+      </button>
+    );
+  }
+  if (type === "btn-pending") {
+    return (
+      <button className="rounded-2xl bg-yellow-800 text-yellow-200 py-1 px-4">
+        {title}
+      </button>
+    );
+  }
+  if (type === "btn-cancel") {
+  }
   if (type === "success") {
     return (
       <button className="w-full mt-5 px-4 py-5 rounded-md text-center bg-green-500 shadow-lg text-white font-semibold mr-3">
