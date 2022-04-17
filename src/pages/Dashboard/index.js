98d17248e9b3cf7a3a94ee0ce9ac540f879f4db5
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-04-17 16:32:14 +0700

    add data report and header

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,20 +1,19 @@
 import React from "react";
 import { Navbar } from "../../components";
+import Header from "./Header";
+import OrderReport from "./Order-Report";
 
 function Dashboard() {
   return (
-    <div className="flex flex-row min-h-screen bg-gray-100">
+    <div className="flex flex-row min-h-screen bg-slate-100">
       <Navbar />
       <div className="w-full flex lg:flex-row flex-col">
-        <div className="w-full pt-12 px-16 container mx-auto">
-          <div className="text-2xl">Dashboard Page</div>
+        <div className="w-full pt-12 px-5 container mx-auto">
+          <Header />
+          {/* order report */}
+          <OrderReport />
         </div>
-        <div className="w-full pt-12 px-16 container mx-auto">
-          <div className="text-2xl">Dashboard Page</div>
-        </div>
-        <div className="w-full pt-12 px-16 container mx-auto">
-          <div className="text-2xl">Dashboard Page</div>
-        </div>
+        <div className="w-full lg:w-1/2 pt-12 px-12"></div>
       </div>
     </div>
   );
