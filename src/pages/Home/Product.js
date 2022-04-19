af33c25a7d9134a100e7b42754979a0dfbd27576
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-04-19 16:29:46 +0700

    fix grid responsive

--- src/pages/Home/Product.js
+++ src/pages/Home/Product.js
@@ -10,7 +10,7 @@ const Product = () => {
 
   return (
     <>
-      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 ">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 ">
         {product.map((item) => {
           return (
             <ProductItem
