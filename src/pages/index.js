9df59cdd7ace0cf301b2fb6aea1095af03a04214
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-04-19 16:28:46 +0700

    exports pages profile

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,6 @@ import Home from "./Home";
 import Products from "./Products";
 import Dashboard from "./Dashboard";
 import EmptyPage from "./404";
+import Profile from "./Profile";
 
-export { Home, Products, EmptyPage, Dashboard };
+export { Home, Products, EmptyPage, Dashboard, Profile };
