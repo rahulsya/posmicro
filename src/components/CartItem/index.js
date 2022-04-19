7434c52bc4d71b3d52ea821688424d4cb4f16831
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-04-19 16:27:51 +0700

    add text amount carts

--- src/components/CartItem/index.js
+++ src/components/CartItem/index.js
@@ -17,20 +17,23 @@ const CartItem = ({ item, onInc, onDec }) => {
               <p className="font-semibold">{item.name}</p>
               <p className="font-bold">Rp{item.price}</p>
             </div>
-            <div className="flex justify-end items-center text-gray-800 font-semibold">
-              <button
-                onClick={onDec}
-                className="w-9 py-2 rounded-lg text-center bg-gray-400 text-white font-bold"
-              >
-                -
-              </button>
-              <div className="px-4">{item.qty}</div>
-              <button
-                onClick={onInc}
-                className="w-9 py-2 rounded-lg text-center bg-blue-500 text-white font-bold"
-              >
-                +
-              </button>
+            <div className="flex flex-col items-end font-semibold">
+              <div className="pr-8 pb-3">Amount</div>
+              <div className="flex items-center text-gray-800 ">
+                <button
+                  onClick={onDec}
+                  className="w-9 py-2 rounded-lg text-center bg-gray-400 text-white font-bold"
+                >
+                  -
+                </button>
+                <div className="px-4">{item.qty}</div>
+                <button
+                  onClick={onInc}
+                  className="w-9 py-2 rounded-lg text-center bg-blue-500 text-white font-bold"
+                >
+                  +
+                </button>
+              </div>
             </div>
           </div>
         </div>
