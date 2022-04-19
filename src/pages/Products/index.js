51f5b5d001feacc804937f04b9d1b6a275b5bc8f
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-04-19 16:30:43 +0700

    add button to show fromCategory

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Navbar, ProductItem, Button } from "../../components";
 import ProductForm from "./productForm";
+import CategoryForm from "./categoryForm";
 import { product } from "../../data/data";
 
 function Products() {
@@ -9,14 +10,17 @@ function Products() {
   const [showFormEdit, setShowFromEdit] = useState(false);
   const [dataProduct, setDataProduct] = useState({});
 
+  const [showFormCategory, setShowFormCategory] = useState(false);
+
   return (
     <div className="flex flex-row min-h-screen bg-slate-100">
       <Navbar />
       <div className="w-full flex lg:flex-row flex-col">
         <div className="w-full pt-12 px-4 container mx-auto">
-          <div className="text-2xl font-semibold text-gray-700 mb-5">
+          <div className="text-3xl font-bold text-gray-700 mb-5">
             Products Management
           </div>
+          <hr className="pb-3" />
           <div className="actions flex flex-row mb-5">
             <Button
               title="Add Product"
@@ -25,7 +29,15 @@ function Products() {
                 setShowFromEdit(false);
               }}
             />
-            <Button title="Add Category" />
+            <Button
+              onPress={() => {
+                // setShowFromEdit(false);
+                // setShowFormInput(false);
+                setShowFormCategory(true);
+              }}
+              bg="bg-yellow-600"
+              title="Manage Categories"
+            />
           </div>
           <div className="grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 gap-4 ">
             {product.map((item) => {
@@ -61,6 +73,16 @@ function Products() {
             />
           </div>
         ) : null}
+
+        {showFormCategory && (
+          <div className="bg-gray-50 w-full lg:w-2/5 pt-12 px-5 border-l-2">
+            <CategoryForm
+              categoryState={{
+                setShowFormCategory,
+              }}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
