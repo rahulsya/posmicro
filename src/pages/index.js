c866a5380cd14c8b3fe460a0f81b28d6410a11bb
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-04-21 17:23:36 +0700

    add pages login

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,6 @@ import Products from "./Products";
 import Dashboard from "./Dashboard";
 import EmptyPage from "./404";
 import Profile from "./Profile";
+import Login from "./Login";
 
-export { Home, Products, EmptyPage, Dashboard, Profile };
+export { Home, Products, EmptyPage, Dashboard, Profile, Login };
