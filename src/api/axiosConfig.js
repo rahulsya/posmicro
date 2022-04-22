70c3291ef4bd2203939c7e7e9be2a13e3935358c
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-04-22 20:26:59 +0700

    add error handler for axios

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,16 +1,12 @@
 import axios from "axios";
+import errorHandler from "./errorHandler";
 
 const instance = axios.create({
   Accept: "application/json",
   baseURL: `${process.env.REACT_APP_API_GATEWAY_URL}`,
 });
 
-instance.interceptors.response.use(
-  (response) => response.data,
-  (err) => {
-    console.log(err.response);
-  }
-);
+instance.interceptors.response.use((response) => response.data, errorHandler);
 
 export { default as setAuthorizationHeader } from "./setAuthorizationHeader";
 export default instance;
---
+++ src/api/errorHandler.js
@@ -0,0 +1,48 @@
+import axios, { setAuthorizationHeader } from "./axiosConfig";
+import users from "./users";
+
+export default function errorHandler(error) {
+  if (error) {
+    let message;
+    if (error.response) {
+      //   console.log(error.response);
+      const origialRequest = error.config;
+      if (error.response.status === 500)
+        message = "Something went terribly wrong";
+      else if (error.response.status === 403 && !origialRequest._retry) {
+        origialRequest._retry = true;
+        const session = localStorage["tokens"]
+          ? JSON.parse(localStorage["tokens"])
+          : null;
+        return users
+          .refresh({
+            refresh_token: session.refresh_token,
+            email: session.email,
+          })
+          .then((response) => {
+            if (response) {
+              setAuthorizationHeader(response.token);
+              localStorage.setItem(
+                "tokens",
+                JSON.stringify({
+                  ...session,
+                  token: response.token,
+                })
+              );
+              origialRequest.headers.authorization = response.token;
+
+              return axios(origialRequest);
+            } else {
+              window.location.href = "/login";
+              localStorage.removeItem("tokens");
+            }
+          });
+        // console.log(session);
+      } else message = error.response.data.message;
+
+      if (typeof message === "string") console.log("error alert toast here");
+
+      return Promise.reject(error);
+    }
+  }
+}
