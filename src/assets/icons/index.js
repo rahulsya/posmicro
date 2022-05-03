845ff8893ef95fa0b021c0033645406b155ec154
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-05-03 14:34:29 +0700

    add icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -4,5 +4,19 @@ import { ReactComponent as DashboardIC } from "./Dashboard.svg";
 import { ReactComponent as LogoutIC } from "./Logout.svg";
 import { ReactComponent as EmptyIL } from "./emptyIL.svg";
 import { ReactComponent as TrashIC } from "./Trash.svg";
+import { ReactComponent as SearchIC } from "./searchIC.svg";
+import { ReactComponent as OrderIC } from "./OrderIC.svg";
+import { ReactComponent as ProductIC } from "./ProductIC.svg";
+import { ReactComponent as ProfileIC } from "./ProfileIC.svg";
 
-export { HomeIC, LogoutIC, DashboardIC, EmptyIL, TrashIC };
+export {
+  HomeIC,
+  LogoutIC,
+  DashboardIC,
+  EmptyIL,
+  TrashIC,
+  SearchIC,
+  OrderIC,
+  ProductIC,
+  ProfileIC,
+};
