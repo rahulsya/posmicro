22ebe0589d5cc774c90390f59665410be2822266
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-05-06 16:11:55 +0700

    add css toastify

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
+import "react-toastify/dist/ReactToastify.css";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
