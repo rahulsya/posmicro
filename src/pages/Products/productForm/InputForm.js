0db87ad6c91110518d388cabcac49a827f75fb26
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-05-10 22:41:23 +0700

    add feature add data product

--- src/pages/Products/productForm/InputForm.js
+++ src/pages/Products/productForm/InputForm.js
@@ -1,7 +1,32 @@
 import React from "react";
 import { Button, Input } from "../../../components";
+import { useSelector } from "react-redux";
+import { useForm } from "react-hook-form";
+import { addproducts } from "../../../api/products/product";
 
-function InputForm({ setFormInput }) {
+function InputForm({ setFormInput, setDataProduct }) {
+  const category = useSelector((state) => state.categories);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    const payload = new FormData();
+    payload.append("name", data.name);
+    payload.append("price", data.price);
+    payload.append("stock", data.amount_stock);
+    payload.append("category_id", data.category_id);
+    payload.append("image", data.image[0]);
+    addproducts(payload)
+      .then((res) => {
+        setDataProduct((oldState) => [...oldState, res.data]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <>
       <div className="flex flex-row justify-between mb-5">
@@ -13,40 +38,62 @@ function InputForm({ setFormInput }) {
         />
       </div>
       <div>
-        <Input name="name" title="Name" placeholder="Product Name" />
-        <Input
-          name="price"
-          type="number"
-          title="Price"
-          placeholder="product price"
-        />
-        <Input
-          name="stock"
-          type="number"
-          title="Stock"
-          placeholder="Product Stock"
-        />
-        <div className="pb-3">
-          <label className="font-semibold text-gray-800" htmlFor="category_id">
-            Category Product
-          </label>
-          <select
-            className="w-full py-3 px-4 rounded-md p-3 border-[1px]"
-            id="cateogry_id"
-          >
-            <option value="">sticker materials</option>
-            <option value="">sticker materials</option>
-            <option value="">sticker materials</option>
-          </select>
-        </div>
-        <Input
-          name="image"
-          type="file"
-          title="Image"
-          placeholder="Product Image"
-        />
-        <div className="mt-5" />
-        <Button bg="bg-green-500" title="Add Product" />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            {...register("name", { required: true })}
+            name="name"
+            title="Name"
+            placeholder="Product Name"
+            errors={errors.name}
+          />
+          <Input
+            {...register("price", { required: true })}
+            name="price"
+            type="number"
+            title="Price"
+            placeholder="product price"
+            errors={errors.price}
+          />
+          <Input
+            {...register("amount_stock", { required: true })}
+            name="amount_stock"
+            type="number"
+            title="Stock"
+            placeholder="Product Stock"
+            errors={errors.amount_stock}
+          />
+          <div className="pb-3">
+            <label
+              className="font-semibold text-gray-800"
+              htmlFor="category_id"
+            >
+              Category Product
+            </label>
+            <select
+              {...register("category_id", { required: true })}
+              className="w-full py-3 px-4 rounded-md p-3 border-[1px]"
+              id="cateogry_id"
+            >
+              {category?.data.map((item) => {
+                return (
+                  <option key={item.id} value={`${item.id}`}>
+                    {item.name}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <Input
+            {...register("image", { required: true })}
+            name="image"
+            type="file"
+            title="Image"
+            placeholder="Product Image"
+            errors={errors.image}
+          />
+          <div className="mt-5" />
+          <Button bg="bg-green-500" title="Add Product" />
+        </form>
       </div>
     </>
   );
