d99effacae6e1aac2e69fecacdd73b173649b18d
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-05-10 22:33:44 +0700

    add options method shipping and payment

--- src/pages/Home/Carts.js
+++ src/pages/Home/Carts.js
@@ -4,14 +4,37 @@ import { EmptyIL } from "../../assets/icons";
 
 import { useSelector, useDispatch } from "react-redux";
 import { addItem, removeItem, clearItem } from "../../redux/Cart/action";
+import { useNavigate } from "react-router-dom";
 
 import sumPrice from "../../utils/sum-price";
 import { formatNumber } from "../../utils/format-rupiah";
 
+import axios from "axios";
+
 function Carts() {
+  let navigate = useNavigate();
   const dispatch = useDispatch();
   const carts = useSelector((state) => state.cart);
   const [currentPayment, setCurrentPayment] = useState("cash");
+  const [shipment, setShipment] = useState("cod");
+
+  const onSubmit = () => {
+    console.log("test");
+    axios
+      .post(`http://localhost:4003/order`, {
+        products: carts,
+        total_amount: sumPrice(carts),
+        total_shipping: null,
+      })
+      .then((res) => {
+        console.log(res.data);
+        dispatch(clearItem());
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  };
+
   return (
     <>
       <div>
@@ -25,6 +48,7 @@ function Carts() {
             carts.map((item, index) => {
               return (
                 <CartItem
+                  isShowAction
                   onDec={() => dispatch(removeItem(item))}
                   onInc={() => dispatch(addItem(item))}
                   key={index}
@@ -44,32 +68,93 @@ function Carts() {
           <div>Subtotal</div>
           <div>{formatNumber(sumPrice(carts))}</div>
         </div>
-        {/*payment  */}
-        <div className="font-semibold mt-5">Select Payment</div>
-        <div className="flex flex-row py-3">
-          <button
-            onClick={() => setCurrentPayment("cash")}
-            className={`px-4 py-2 rounded-md text-center ${
-              currentPayment === "cash" ? "bg-blue-500" : "bg-gray-400"
-            } shadow-lg text-white font-semibold mr-3`}
-          >
-            Cash
-          </button>
-          <button
-            onClick={() => setCurrentPayment("noncash")}
-            className={`px-4 py-2 rounded-md text-center ${
-              currentPayment === "noncash" ? "bg-blue-500" : "bg-gray-400"
-            } shadow-lg text-white`}
-          >
-            Non Cash
-          </button>
-        </div>
-
+        <ShippingMethod
+          dataState={{ shipment, setShipment, setCurrentPayment }}
+        />
+        <PaymentMethod
+          dataState={{ currentPayment, setShipment, setCurrentPayment }}
+        />
         <hr />
-        <Button type="success" title="Complete Transaction" />
+        <div className="mb-6">
+          {shipment === "cod" ? (
+            <Button
+              onPress={onSubmit}
+              type="success"
+              title="Complete Transactions"
+            />
+          ) : (
+            <Button
+              onPress={() => navigate("/shipment")}
+              type="success"
+              title="Complete Shipment"
+            />
+          )}
+        </div>
       </div>
     </>
   );
 }
+
+const ShippingMethod = ({ dataState }) => {
+  const { shipment, setShipment, setCurrentPayment } = dataState;
+  return (
+    <>
+      {/* shipment */}
+      <div className="font-semibold mt-5">shipping method</div>
+      <div className="flex flex-row py-3">
+        <button
+          onClick={() => setShipment("cod")}
+          className={`px-4 py-2 rounded-md text-center ${
+            shipment === "cod" ? "bg-blue-500" : "bg-gray-400"
+          } shadow-lg text-white font-semibold mr-3`}
+        >
+          COD
+        </button>
+        <button
+          onClick={() => {
+            setCurrentPayment("noncash");
+            setShipment("courier");
+          }}
+          className={`px-4 py-2 rounded-md text-center ${
+            shipment === "courier" ? "bg-blue-500" : "bg-gray-400"
+          } shadow-lg text-white font-semibold mr-3`}
+        >
+          Courier Service
+        </button>
+      </div>
+    </>
+  );
+};
+
+const PaymentMethod = ({ dataState }) => {
+  const { currentPayment, setShipment, setCurrentPayment } = dataState;
+  return (
+    <>
+      {/*payment  */}
+      <div className="font-semibold mt-5">Select Payment</div>
+      <div className="flex flex-row py-3">
+        <button
+          onClick={() => {
+            setCurrentPayment("cash");
+            setShipment("cod");
+          }}
+          className={`px-4 py-2 rounded-md text-center ${
+            currentPayment === "cash" ? "bg-blue-500" : "bg-gray-400"
+          } shadow-lg text-white font-semibold mr-3`}
+        >
+          Cash
+        </button>
+        <button
+          onClick={() => setCurrentPayment("noncash")}
+          className={`px-4 py-2 rounded-md text-center ${
+            currentPayment === "noncash" ? "bg-blue-500" : "bg-gray-400"
+          } shadow-lg text-white`}
+        >
+          Non Cash
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Carts;
