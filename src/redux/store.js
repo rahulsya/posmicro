ed91f0a2a7ba2c5c8ee54add1d117adcf69b45eb
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-05-10 22:43:30 +0700

    add redux thunk and redux devtools debugger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,17 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import cartReducer from "./Cart/reducer";
-
+import categoriesReducer from "./Categories/reducer";
+// debug mode
+const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const rootReducers = combineReducers({
   cart: cartReducer,
+  categories: categoriesReducer,
 });
 
-const store = createStore(rootReducers);
+const store = createStore(
+  rootReducers,
+  composerEnhancer(applyMiddleware(thunk))
+);
 
 export default store;
