7bac0cf93f05cc3b4fbb920d95c70a423460847a
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-05-10 22:35:57 +0700

    implement api products

--- src/pages/Home/Product.js
+++ src/pages/Home/Product.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ProductItem } from "../../components";
 
-import { product } from "../../data/data";
 import { useDispatch } from "react-redux";
 import { addItem } from "../../redux/Cart/action";
+
+import { products } from "../../api/products/product";
 
 const Product = () => {
   const dispatch = useDispatch();
 
+  const [dataProducts, setDataProducts] = useState([]);
+
+  useEffect(() => {
+    products()
+      .then((response) => {
+        setDataProducts(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 ">
-        {product.map((item) => {
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 ">
+        {dataProducts?.map((item) => {
           return (
             <ProductItem
               onClick={() => dispatch(addItem(item))}
