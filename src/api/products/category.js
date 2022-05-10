06a19da5a1b91bbb65ed1eebb4a01dbcbf18496d
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-05-10 22:32:43 +0700

    add endpoint categories

--- src/api/products/category.js
+++ src/api/products/category.js
@@ -1,14 +1,14 @@
 import axios from "../axiosConfig";
 
 export const categories = async () => {
-  return await axios.get(`/products/category`);
+  return await axios.get(`/categories`);
 };
 export const addCategory = async (data) => {
-  return await axios.post(`/products/category`, data);
+  return await axios.post(`/categories`, data);
 };
-export const editCategory = async (data) => {
-  return await axios.put(`/products/category`, data);
+export const editCategory = async (id, data) => {
+  return await axios.put(`/categories/${id}`, data);
 };
 export const deleteCategory = async (id) => {
-  return await axios.delete(`/products/category/${id}`);
+  return await axios.delete(`/categories/${id}`);
 };
