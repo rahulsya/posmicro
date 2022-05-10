732789af583dbe299eb118b972051c28ce25a537
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-05-10 22:39:33 +0700

    implement api to edit data product

--- src/pages/Products/productForm/editForm.js
+++ src/pages/Products/productForm/editForm.js
@@ -1,13 +1,36 @@
+import { useState } from "react";
 import { Input, Button } from "../../../components";
+import { useSelector } from "react-redux";
+import { editProduct } from "../../../api/products/product";
+import toast from "../../../utils/toast";
 
 function EditForm({ data, setShowFromEdit, setData }) {
-  // console.log(data);
+  const [newImage, setNewImage] = useState(null);
+
+  const category = useSelector((state) => state.categories);
   const handeOnchange = (e) => {
     return setData((state) => ({
       ...state,
       [e.target.name]: e.target.value,
     }));
   };
+
+  const onSubmit = () => {
+    const payload = new FormData();
+    payload.append("name", data.name);
+    payload.append("price", data.price);
+    payload.append("stock", data.amount_stock);
+    payload.append("category_id", data.category_id);
+    if (newImage) {
+      payload.append("image", newImage);
+    }
+    editProduct(data.id, payload)
+      .then((response) => {
+        toast("success", "update product success");
+      })
+      .catch((err) => console.log(err.message));
+  };
+
   return (
     <>
       <div className="flex flex-row justify-between mb-5">
@@ -35,21 +58,45 @@ function EditForm({ data, setShowFromEdit, setData }) {
           onChange={handeOnchange}
         />
         <Input
-          name="stock"
+          name="amount_stock"
           type="number"
           title="Stock"
           placeholder="Product Stock"
-          value={data?.stock}
+          value={data?.amount_stock}
           onChange={handeOnchange}
         />
+        <div className="pb-3">
+          <label className="font-semibold text-gray-800" htmlFor="category_id">
+            Category Product
+          </label>
+          <select
+            name="category_id"
+            onChange={handeOnchange}
+            className="w-full py-3 px-4 rounded-md p-3 border-[1px]"
+            id="cateogry_id"
+          >
+            {category?.data.map((item) => {
+              return (
+                <option
+                  selected={data.category_id == item.id ? true : false}
+                  key={item.id}
+                  value={`${item.id}`}
+                >
+                  {item.name}
+                </option>
+              );
+            })}
+          </select>
+        </div>
         <Input
           name="image"
           type="file"
           title="Image"
           placeholder="Product Image"
+          onChange={(e) => setNewImage(e.target.files[0])}
         />
         <div className="mt-5" />
-        <Button bg="bg-green-500" title="Update Product" />
+        <Button onPress={onSubmit} bg="bg-green-500" title="Update Product" />
       </div>
     </>
   );
