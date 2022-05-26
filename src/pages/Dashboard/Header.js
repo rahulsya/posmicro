ceb3ba1034c44ded08bee8222113e43f15de66b3
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-05-26 13:23:02 +0700

    add date format

--- src/pages/Dashboard/Header.js
+++ src/pages/Dashboard/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { HomeIC, DashboardIC } from "../../assets/icons";
+import formatNumber from "dateformat";
 
 function Header() {
   return (
@@ -7,7 +8,7 @@ function Header() {
       <div className="flex flex-col font-bold text-3xl">
         <div className="text-gray-700 pb-2">Dashboard</div>
         <div className="text-gray-600 text-sm font-normal pb-5">
-          Saturday, 16 April 2022
+	  {formatNumber(new Date(),"fullDate")}
         </div>
         <hr />
       </div>
