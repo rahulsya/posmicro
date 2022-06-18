43ad2ba4664894a42220cd64a155851d7a2a0b36
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-06-18 23:05:27 +0700

    add props onclick

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,14 +3,20 @@ import React from "react";
 function Button({ title, type, bg, color, onPress, disabled = false }) {
   if (type === "btn-completed") {
     return (
-      <button className="rounded-2xl bg-green-800 text-green-200 py-1 px-4">
+      <button
+        onClick={onPress}
+        className="rounded-2xl bg-green-800 text-green-200 py-1 px-4"
+      >
         {title}
       </button>
     );
   }
   if (type === "btn-pending") {
     return (
-      <button className="rounded-2xl bg-yellow-800 text-yellow-200 py-1 px-4">
+      <button
+        onClick={onPress}
+        className="rounded-2xl bg-yellow-800 text-yellow-200 py-1 px-4"
+      >
         {title}
       </button>
     );
@@ -20,6 +26,7 @@ function Button({ title, type, bg, color, onPress, disabled = false }) {
   if (type === "success") {
     return (
       <button
+        data-testid="button-test"
         onClick={onPress}
         className="hover:bg-green-500 w-full mt-5 px-4 py-5 rounded-md text-center bg-green-500 shadow-lg text-white font-semibold mr-3"
       >
