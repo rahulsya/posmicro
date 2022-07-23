eb7ff1a161a91c74a8eb37f2f5bb0bfb60ebd45c
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-07-23 15:22:33 +0700

    add pages reset password & request reset password

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,4 +9,6 @@ import Shipment from "./Shipment";
 export { default as OrderHistory } from "./OrderHistory";
 export { default as DetailOrderHistory } from "./DetailOrderHistory";
 export { default as Register } from "./Register";
+export { default as ResetPassword } from "./ResetPassword";
+export { default as RequestResetPassword } from "./RequestResetPassword";
 export { Home, Products, EmptyPage, Dashboard, Profile, Login, Shipment };
